11349b9c34f916c98a7cbbc0e7bb48a58464c49b
Matias <matiascisterna39@gmail.com>
2023-07-25 15:28:07 -0400

    commit obligatorio

--- pucaraweb/settings.py
+++ pucaraweb/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pucara',
         'USER': 'root',
-        'PASSWORD': 'ottotorapilontai',
+        'PASSWORD': 'matiloko16',
         'HOST': 'localhost',
         'PORT': '',
     }
