304f4aaaabe2c15c5ec688f729c8e75495bb944a
Franco Marchese <franco.marchese@sulz.cl>
2023-07-24 21:12:02 -0400

    commit obligatorio

--- appCentral/urls.py
+++ appCentral/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import MenuView, VerRegistrosView, NuevoRegistroView
+from .views import VerRegistrosView, NuevoRegistroView
 
 
 urlpatterns = [
-    # path("menu/", MenuView.as_view(), name='menu'),
     path("registros/", VerRegistrosView.as_view(), name='registros'),
     path("registros/nuevo/", NuevoRegistroView.as_view(), name='nuevoRegistro'),
 ]
